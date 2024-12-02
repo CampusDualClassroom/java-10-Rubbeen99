1dc7e1402d77d4e49bfebb629f2b83267a5ec97e
rubbeen99 <ruben20042000@gmail.com>
2024-12-02 11:54:05 +0100

    Ejercicio.10 resuelto.

--- src/main/java/com/campusdual/classroom/Exercise10.java
+++ src/main/java/com/campusdual/classroom/Exercise10.java
@@ -4,26 +4,47 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Exercise10 {
 
-    //TODO ↓
+
     // Imprimir el color de las pelotas que salgan, hasta que salgan 2 de color azul
     // El mensaje a imprimir es → La bola es de color: COLOR_BOLA
     // COLOR_BOLA puede ser → rojo, azul, verde
     public static void main(String[] args) {
+        int contadorBolaazul = 0;
 
+        while (contadorBolaazul < 2) {
+            String colorBola = getBall();
+            System.out.println("La bola es de color: " + colorBola);
+            if (colorBola.equals("azul")) {
+                contadorBolaazul++;
+            }
+        }
     }
 
-    //TODO ↓
-    // Un método que devuelva una cadena de texto con el color de una pelota (en minúscula). Dicho color se escogerá de
-    // manera aleatoria. Se empleará el método randomWithRange(int min, int max) para escoger un número aleatorio, que
+
+    // Un metodo que devuelva una cadena de texto con el color deD una pelota (en minúscula). Dicho color se escogerá de
+    // manera aleatoria. Se empleará el metodo randomWithRange(int min, int max) para escoger un numero aleatorio, que
     // lo asociaremos al valor de un color de la siguiente manera:
     // 1 → rojo
     // 2 → azul
     // 3 → verde
     public static String getBall() {
-        return null;
+        int numeroAleatorio = randomWithRange(1, 4);
+
+        switch (numeroAleatorio) {
+            case 1:
+                return "rojo";
+            case 2:
+                return "azul";
+            case 3:
+                return "verde";
+            default:
+                return "rojo";
+        }
     }
 
+
+
     public static int randomWithRange(int min, int max) {
-        return ThreadLocalRandom.current().nextInt(min,max);
+        return ThreadLocalRandom.current().nextInt(min, max);
     }
 }
